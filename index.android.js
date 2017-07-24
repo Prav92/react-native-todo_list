c397ef98ae883467396e00caf51319f16e2775ad
Praveen Kumar <cbkmar92@gmail.com>
2017-07-24 17:30:03 +0530

    sample react native with components

---
+++ index.android.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native'
+
+// Import the App container component
+import App from './components/App'
+
+AppRegistry.registerComponent('ToDOApp', () => App);
